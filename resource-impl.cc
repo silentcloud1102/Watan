e9f8ff7678cac31f7ecef06c75788f19008103cf
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-11-24 18:18:25 -0500

    resource work

---
+++ resource-impl.cc
@@ -0,0 +1,22 @@
+module Resource;
+
+
+import <string>;
+
+
+int caffeine;
+int lab;
+int lecture;
+int study;
+int tutorial;
+
+Resource(int caffeine = 0, int lab = 0,  int lecture = 0, int study = 0, int tutorial = 0);
+
+Resource operator+(Resource &other);
+Resource operator-(Resource &other);
+
+Resource& operator-=(Resource &other);
+Resource& operator+=(Resource &other);
+auto operator<=>(const Resource &other) const = default;
+
+int count();
