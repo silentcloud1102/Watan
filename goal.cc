ca64b4e7f123ef6c17c34a40d188c703b24c60b9
e73lin <e73lin@uwaterloo.ca>
2025-11-24 18:42:59 -0500

    student

--- goal.cc
+++ goal.cc
@@ -3,27 +3,29 @@ export module Goal;
 import ISubject;
 
 export class Goal : public ISubject {
+    vector<int> adjacent_course_criterion;
+    vector<int> adjacent_goals;
+
     Goal(int location): 
+    string getnum() const {
+      if (owner != "") {
+          return owner;
+        }
+
+          if (number < 10) {
+          return " " + to_string(number);
+          }
+          return to_string(number);
+    }
 };
 
 class goal {
   public:
     int number;
     string owner;
-    vector<int> adjacent_course_criterion;
-    vector<int> adjacent_goals;
+    
     
       goal(int num, string str) : number(num), owner(str) {}
       void addAdjacentcourse_criterion(const vector<int> &v) { adjacent_course_criterion = v; }
       void addAdjacentgoal(const vector<int> &e) { adjacent_goals = e; }
-      string getnum() const {
-      if (owner != "") {
-          return owner;
-          }
-
-          if (number < 10) {
-          return " " + to_string(number);
-          }
-          return to_string(number);
-      }
 };
