a71b05052b5309490c4f9a1a716ba93a6f856316
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-11-26 19:45:09 -0500

    added documentation, argument naming, acquire method

--- goal.cc
+++ goal.cc
@@ -6,22 +6,30 @@ import ISubject;
 
 export class Goal : public ISubject {
   char owner;
-  vector<int> adjacent_course_criterion;
+  vector<int> adjacent_criteria;
   vector<int> adjacent_goals;
   public:
     explicit Goal(int location); 
 
+    // "subscription" method
+    void acquire(IObserver* student) override;
+   
+
     // Printing method
     std::string getnum() const;
+    char get_owner();
+
+    // Buy logic methods:
+    Resource & cost();
+    bool owned();
+    bool adjacent(vector<int> &criteria, vector<int> &goals) const;
 
     // Adjacency Methods
     bool adjacent_goal_check(int location) const;
     bool adjacent_criteria_check(int location) const;
-    void addAdjacentcourse_criterion(const vector<int> &v);
-    void addAdjacentgoal(const vector<int> &e);
+    void set_adjacent_criteria(const vector<int> & criteria);
+    void set_adjacent_goals(const vector<int> & goals);
 
+    // Saving methods
     std::string get_save_string() const;
-    bool owned();
-    char get_owner();
-    Resource cost();
 };
