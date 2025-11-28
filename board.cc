c2748dcd0df19c21936a553e3bb8f633b3a7bc91
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-11-28 02:39:59 -0500

    moved constants to class as static constants, added documentation

--- board.cc
+++ board.cc
@@ -9,7 +9,24 @@ import Goal;
 import Tile;
 
 export class Board {
+
+    // static constant config values for board initialization
+    static const std::vector<std::vector<int>> GoalsAdjCriterion;
+    static const std::vector<std::vector<int>> GoalsAdjGoals;
+    static const std::vector<std::vector<int>> CriterionAdjCriterion;
+    static const std::vector<std::vector<int>> CriterionAdjGoals;
+    static const std::vector<std::vector<int>> tilegoals;
+
+    // resource names to pass to Tiles
+    static const std::vector<std::string> resource_names;
+
+    // constants for the desired sizes, to initialize and improve readability
+    static const int num_of_goals = 72;
+    static const int num_of_course_criteria = 54;
+    static const int num_of_tiles = 19;
+
     int goose_tile;
+    
     // std::vector<Tile> tiles;
     // std::vector<Goal> goals;
     // std::vector<Criteria> course_criteria;
@@ -18,15 +35,27 @@ export class Board {
     std::vector<std::unique_ptr<Criteria>> course_criteria;
     
     std::string goose_printer(std::string tile) const;
+
     public:
+        // constructor
         Board(unsigned seed);
-        void load_saveData(std::vector<int> save_data);
-        std::string saveData();
+
+        // update tiles passes on notifications to criteria (Subjects observed by Player)
         void update_tiles(int roll_num) const;
+
+        // get methods that return pointers, for Student to access and acquire/upgrade
         Criteria* getCriteria(int criteria_num) const;
         Goal* getGoal(int goal_num) const;
+
+        // goose handling
         void updateGoose(int new_goosetile);
         int getGooseTile() const;
+
+        // output
         friend std::ostream &operator<<(std::ostream &out, const Board &b);
+
+        // save methods
+        void load_saveData(std::vector<int> save_data);
+        std::string saveData();
 };
 
