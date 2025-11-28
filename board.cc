57961d2fe981d57aaca3b09c492306ca12bad1db
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-11-27 23:11:09 -0500

    using a vector of unique pointer instead for access to memory in a more stable manner

--- board.cc
+++ board.cc
@@ -3,23 +3,28 @@ export module Board;
 import <iostream>;
 import <string>;
 import <vector>;
+import <memory>;
 import Criteria;
 import Goal;
 import Tile;
 
 export class Board {
     int goose_tile;
-    std::vector<Tile> tiles;
-    std::vector<Goal> goals;
-    std::vector<Criteria> course_criteria;
+    // std::vector<Tile> tiles;
+    // std::vector<Goal> goals;
+    // std::vector<Criteria> course_criteria;
+    std::vector<std::unique_ptr<Tile>> tiles;
+    std::vector<std::unique_ptr<Goal>> goals;
+    std::vector<std::unique_ptr<Criteria>> course_criteria;
+    
     std::string goose_printer(std::string tile) const;
     public:
         Board(unsigned seed);
         void load_saveData(std::vector<int> save_data);
         std::string saveData();
         void update_tiles(int roll_num) const;
-        const Criteria& getCriteria(int criteria_num) const;
-        const Goal& getGoals(int goal_num) const;
+        Criteria* getCriteria(int criteria_num) const;
+        Goal* getGoals(int goal_num) const;
         void updateGoose(int new_goosetile);
         int getGooseTile() const;
         friend std::ostream &operator<<(std::ostream &out, const Board &b);
